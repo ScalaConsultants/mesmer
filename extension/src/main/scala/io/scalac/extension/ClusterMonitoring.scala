6c6e741879271f54b21975100afdf0a1b9f52226
Piotr Jósiak <piotr.josiak@scalac.io>
2020-11-01 12:39:23 +0100

    Change initialization

--- extension/src/main/scala/io/scalac/extension/ClusterMonitoring.scala
+++ extension/src/main/scala/io/scalac/extension/ClusterMonitoring.scala
@@ -1,67 +1,34 @@
 package io.scalac.extension
 
-import akka.actor.ExtendedActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{
   ActorSystem,
   Extension,
   ExtensionId,
   SupervisorStrategy
 }
-import akka.actor.typed.scaladsl.Behaviors
-import akka.cluster.typed.{Cluster, ClusterSingleton, SingletonActor}
-import com.typesafe.config.{Config, ConfigException}
-import io.opentelemetry.metrics.LongUpDownCounter
-
-import scala.reflect.{ClassTag, classTag}
-import scala.util.Try
-import scala.collection.JavaConverters._
-
-case class ClusterMonitoringConfig(boot: BootSettings, regions: List[String])
-
-case class BootSettings(bootMemberEvent: Boolean,
-                        bootReachabilityEvents: Boolean)
-
-object ClusterMonitoringConfig {
-
-  import config.ConfigurationUtils._
-
-  def apply(config: Config): ClusterMonitoringConfig = {
-
-    config
-      .tryValue("io.scalac.akka-cluster-monitoring")(_.getConfig)
-      .map { monitoringConfig =>
-        val bootMember =
-          monitoringConfig.tryValue("boot.member")(_.getBoolean).getOrElse(true)
-
-        val bootReachability =
-          config.tryValue("boot.reachability")(_.getBoolean).getOrElse(true)
-
-        val initRegions = monitoringConfig
-          .tryValue("shard-regions")(config => path => config.getStringList(path).asScala.toList)
-          .getOrElse(Nil)
-
-        ClusterMonitoringConfig(
-          BootSettings(bootMember, bootReachability),
-          initRegions
-        )
-      }
-      .getOrElse(ClusterMonitoringConfig(BootSettings(true, true), Nil))
-  }
-
-}
+import akka.cluster.typed.{ClusterSingleton, SingletonActor}
+import io.scalac.extension.config.ClusterMonitoringConfig
 
 object ClusterMonitoring extends ExtensionId[ClusterMonitoring] {
   override def createExtension(system: ActorSystem[_]): ClusterMonitoring = {
-    val monitor = new ClusterMonitoring(system)
+    val config = ClusterMonitoringConfig.apply(system.settings.config)
+    val monitor = new ClusterMonitoring(system, config)
+    import config.boot._
+
+    if (bootMemberEvent) {
+      monitor.startMemberMonitor()
+    }
+    if (bootReachabilityEvents) {
+      monitor.startReachabilityMonitor()
+    }
     monitor
   }
 }
 
-class ClusterMonitoring(private val system: ActorSystem[_]) extends Extension {
-
-//  private lazy val cluster = Cluster(system)
-
-  private val config = ClusterMonitoringConfig(system.settings.config)
+class ClusterMonitoring(private val system: ActorSystem[_],
+                        val config: ClusterMonitoringConfig)
+    extends Extension {
 
   import system.log
 
@@ -89,13 +56,5 @@ class ClusterMonitoring(private val system: ActorSystem[_]) extends Extension {
           "MemberMonitoringActor"
         )
       )
-  }
-
-  // initialization
-  {
-    if (config.boot.bootMemberEvent)
-      startMemberMonitor()
-    if (config.boot.bootMemberEvent)
-      startReachabilityMonitor()
   }
 }
---
+++ extension/src/main/scala/io/scalac/extension/config/ClusterMonitoringConfig.scala
@@ -0,0 +1,39 @@
+package io.scalac.extension.config
+
+import com.typesafe.config.Config
+import io.scalac.extension.config
+import scala.collection.JavaConverters._
+
+case class ClusterMonitoringConfig(boot: BootSettings, regions: List[String])
+
+case class BootSettings(bootMemberEvent: Boolean,
+                        bootReachabilityEvents: Boolean)
+
+object ClusterMonitoringConfig {
+
+  import config.ConfigurationUtils._
+
+  def apply(config: Config): ClusterMonitoringConfig = {
+
+    config
+      .tryValue("io.scalac.akka-cluster-monitoring")(_.getConfig)
+      .map { monitoringConfig =>
+        val bootMember =
+          monitoringConfig.tryValue("boot.member")(_.getBoolean).getOrElse(true)
+
+        val bootReachability =
+          config.tryValue("boot.reachability")(_.getBoolean).getOrElse(true)
+
+        val initRegions = monitoringConfig
+          .tryValue("shard-regions")(config => path => config.getStringList(path).asScala.toList)
+          .getOrElse(Nil)
+
+        ClusterMonitoringConfig(
+          BootSettings(bootMember, bootReachability),
+          initRegions
+        )
+      }
+      .getOrElse(ClusterMonitoringConfig(BootSettings(true, true), Nil))
+  }
+
+}
