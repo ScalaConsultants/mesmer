856a0416c348a8821082e4fd459bb6a0afbd9be6
Piotr Jósiak <piotr.josiak@scalac.io>
2020-11-16 08:09:07 +0100

    Remove event wrapper

--- core/src/main/scala/io/scalac/extension/event/EventBus.scala
+++ core/src/main/scala/io/scalac/extension/event/EventBus.scala
@@ -2,9 +2,10 @@ package io.scalac.agent.event
 import akka.actor.typed.receptionist.Receptionist
 import akka.actor.typed.receptionist.Receptionist.Find
 import akka.actor.typed.scaladsl.AskPattern._
-import akka.actor.typed.{ ActorSystem, Extension, ExtensionId }
-import akka.stream.scaladsl.{ Sink, Source }
-import akka.stream.{ OverflowStrategy, QueueOfferResult }
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Extension, ExtensionId}
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{OverflowStrategy, QueueOfferResult}
 import akka.util.Timeout
 import io.scalac.extension.event.MonitoredEvent
 
@@ -29,21 +30,16 @@ object EventBus extends ExtensionId[EventBus] {
   }
 }
 
-object ReceptionistBasedEventBus {
-  final case class Event(timestamp: Long, event: MonitoredEvent)
-}
 
 private[event] class ReceptionistBasedEventBus(
   implicit val system: ActorSystem[_],
   val ec: ExecutionContext,
   val timeout: Timeout
 ) extends EventBus {
-  import ReceptionistBasedEventBus._
   import system.log
 
   override def publishEvent[T <: MonitoredEvent: ClassTag](event: T): Unit = {
-    val timestamp = System.currentTimeMillis()
-    internalBus.offer(Event(timestamp, event)).onComplete {
+    internalBus.offer(event).onComplete {
       case Success(QueueOfferResult.Enqueued) => log.trace("Successfully enqueued event {}", event)
       case _                                  => log.error("Error occurred when enqueueing event {}", event)
     }
@@ -51,9 +47,9 @@ private[event] class ReceptionistBasedEventBus(
 
   private[this] lazy val internalBus =
     Source
-      .queue[Event](1024, OverflowStrategy.fail)
+      .queue[MonitoredEvent](1024, OverflowStrategy.fail)
       .to(Sink.foreachAsync(1) {
-        case Event(_, event) =>
+        case event  =>
           (Receptionist(system).ref ? Find(event.serviceKey))
             .map(_.serviceInstances(event.serviceKey).filter(_.path.address.hasLocalScope).foreach { ref =>
               ref ! event.asInstanceOf[event.Service]
