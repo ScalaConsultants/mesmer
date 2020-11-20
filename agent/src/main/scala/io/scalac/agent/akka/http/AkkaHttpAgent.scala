5f9952302c92d77b3f74bcd661e6e518ba781283
Piotr Jósiak <piotr.josiak@scalac.io>
2020-11-20 09:10:06 +0100

    Detect akka http version with regex

--- agent/src/main/scala/io/scalac/agent/akka/http/AkkaHttpAgent.scala
+++ agent/src/main/scala/io/scalac/agent/akka/http/AkkaHttpAgent.scala
@@ -1,19 +1,36 @@
 package io.scalac.agent.akka.http
 
 import java.lang.instrument.Instrumentation
+import java.net.URLClassLoader
 
 import io.scalac.agent.Agent
+import net.bytebuddy.agent.builder.AgentBuilder
 import net.bytebuddy.description.`type`.TypeDescription
 import net.bytebuddy.description.method.MethodDescription
 import net.bytebuddy.implementation.MethodDelegation
 import net.bytebuddy.matcher.ElementMatchers
 import net.bytebuddy.matcher.ElementMatchers._
-import net.bytebuddy.agent.builder.AgentBuilder
 
 object AkkaHttpAgent {
+  val akkaHttpRegex = "^akka-http_(.+?)-(.+?)\\.jar$".r
+
+  private def detectAkkaHttpVersion(loader: ClassLoader): Option[String] =
+    loader match {
+      case urLClassLoader: URLClassLoader =>
+        urLClassLoader.getURLs
+          .find(_.getFile.startsWith("akka-http"))
+          .flatMap { url =>
+            url.getFile match {
+              case akkaHttpRegex(_, akkaHttpVersion) => Some(akkaHttpVersion)
+              case _                                 => None
+            }
+          }
+      case _ =>
+        None
+    }
 
   private val routeAgent = Agent(
-    (agentBuilder: AgentBuilder, instrumentation: Instrumentation) => {
+    (agentBuilder: AgentBuilder, instrumentation: Instrumentation) =>
       agentBuilder
         .`type`(
           ElementMatchers.nameEndsWithIgnoreCase[TypeDescription](
@@ -29,27 +46,37 @@ object AkkaHttpAgent {
             )
             .intercept(MethodDelegation.to(classOf[RouteInstrumentation]))
         }
-        .installOn(instrumentation)
-    },
+        .installOn(instrumentation),
     () => ()
   )
 
   private val httpAgent = Agent(
     (agentBuilder: AgentBuilder, instrumentation: Instrumentation) => {
+      @volatile
+      var shouldCheckLibraryVersion = true
       agentBuilder
         .`type`(
           ElementMatchers.nameEndsWithIgnoreCase[TypeDescription](
             "akka.http.scaladsl.HttpExt"
           )
         )
-        .transform { (builder, _, _, _) =>
-          builder
-            .method(
-              (named[MethodDescription]("bindAndHandle")
-                .and(isMethod[MethodDescription])
-                .and(not(isAbstract[MethodDescription])))
-            )
-            .intercept(MethodDelegation.to(classOf[HttpInstrumentation]))
+        .transform {
+          (builder, typeDescription, classLoader, _) =>
+            if (shouldCheckLibraryVersion) {
+              detectAkkaHttpVersion(classLoader).fold {
+                println("Could't detect akka-http version")
+              }(version => println(s"Found akka http version: ${version}"))
+              shouldCheckLibraryVersion = false
+            }
+            typeDescription.getTypeManifestation
+
+            builder
+              .method(
+                (named[MethodDescription]("bindAndHandle")
+                  .and(isMethod[MethodDescription])
+                  .and(not(isAbstract[MethodDescription])))
+              )
+              .intercept(MethodDelegation.to(classOf[HttpInstrumentation]))
         }
         .installOn(instrumentation)
     },
@@ -58,5 +85,3 @@ object AkkaHttpAgent {
 
   val agent = httpAgent
 }
-
-
