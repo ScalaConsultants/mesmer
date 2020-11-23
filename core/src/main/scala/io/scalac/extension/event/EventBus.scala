6d02eca3de7a4818a39e3003468fa3999650496e
Piotr Jósiak <piotr.josiak@scalac.io>
2020-11-23 09:31:21 +0100

    Change logging levels and messages

--- core/src/main/scala/io/scalac/extension/event/EventBus.scala
+++ core/src/main/scala/io/scalac/extension/event/EventBus.scala
@@ -34,6 +34,7 @@ object ReceptionistBasedEventBus {
   def cachingBehavior[T](serviceKey: ServiceKey[T])(implicit timeout: Timeout): Behavior[T] =
     Behaviors
       .setup[Any] { ctx =>
+        ctx.log.debug("Subscribe to service {}", serviceKey)
         Receptionist(ctx.system).ref ! Subscribe(
           serviceKey,
           ctx.messageAdapter { key =>
@@ -44,29 +45,27 @@ object ReceptionistBasedEventBus {
 
         def withCachedServices(services: Set[ActorRef[T]]): Behavior[Any] =
           Behaviors.withStash(1024)(buffer =>
-            Behaviors.receive {
-              case (ctx, message) =>
-                message match {
-                  case Subscribers(refs) => {
-                    ctx.log.info("Subscribers updated")
-                    buffer.unstashAll(withCachedServices(services ++ refs.asInstanceOf[Set[ActorRef[T]]]))
-                  }
-                  case event: T
-                      if services.nonEmpty => { // T is removed on runtime but placing it here make type downcast
-                    services.foreach(_ ! event)
-                    Behaviors.same
-                  }
-                  case event => {
-                    ctx.log.warn("Received event but no services registered for this key")
-                    buffer.stash(event)
-                    Behaviors.same
-                  }
-                }
+            Behaviors.receiveMessage {
+              case Subscribers(refs) => {
+                ctx.log.debug("Subscribers for service {} updated", serviceKey)
+                buffer.unstashAll(withCachedServices(services ++ refs.asInstanceOf[Set[ActorRef[T]]]))
+              }
+              case event: T if services.nonEmpty => { // T is removed on runtime but placing it here make type downcast
+                ctx.log.trace("Publish event for service {}", serviceKey)
+                services.foreach(_ ! event)
+                Behaviors.same
+              }
+              case event => {
+                ctx.log.warn("Received event but no services registered for key {}", serviceKey)
+                buffer.stash(event)
+                Behaviors.same
+              }
+
             }
           )
         withCachedServices(Set.empty)
       }
-      .narrow[T]
+      .narrow[T] // this mimic well union types but might fail if interceptor are in place
 
 }
 
@@ -83,7 +82,7 @@ private[event] class ReceptionistBasedEventBus(
 
   override def publishEvent[T <: AbstractEvent](event: T)(implicit service: Service[event.Service]): Unit = {
     val ref: ActorRef[event.Service] = serviceBuffers.getOrCreate(service) {
-      system.log.error("Initialize event buffer for service {}", service.serviceKey)
+      system.log.debug("Initialize event buffer for service {}", service.serviceKey)
       system.systemActorOf(cachingBehavior(service.serviceKey), UUID.randomUUID().toString)
     }
     ref ! event
