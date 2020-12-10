ac3a23c601f186f9e29f69b9a79c157faa0c28ea
Piotr Jósiak <piotr.josiak@scalac.io>
2020-12-10 08:31:46 +0100

    Fix caching parameters

--- extension/src/main/scala/io/scalac/extension/metric/CachingMonitor.scala
+++ extension/src/main/scala/io/scalac/extension/metric/CachingMonitor.scala
@@ -1,14 +1,14 @@
 package io.scalac.extension.metric
 import org.slf4j.LoggerFactory
 
-import scala.collection.mutable.{ Map => MutableMap }
+import scala.collection.mutable.{Map => MutableMap}
 
-class CachingMonitor[L, T <: Bindable[L]](val monitor: T) extends Bindable[L] {
+class CachingMonitor[L <: AnyRef, T <: Bindable[L]](val monitor: T) extends Bindable[L] {
   private[this] val logger = LoggerFactory.getLogger(this.getClass)
 
-  private[this] val cachedMonitors: MutableMap[L, monitor.Bound] = MutableMap.empty
+  private[this] val cachedMonitors: MutableMap[L, Bound] = MutableMap.empty
 
-  override type Bound = monitor.Bound
+  override type Bound = T#Bound
 
   override final def bind(node: L): Bound =
     cachedMonitors.getOrElse(node, updateMonitors(node, monitor.bind(node)))
@@ -36,5 +36,6 @@ object CachingMonitor {
    * Due to limitations of type interference type parameters has to be manually assigned
    * see https://github.com/scala/bug/issues/5298
    */
-  def caching[L, V <: Bindable[L]](monitor: V) = new CachingMonitor[L, V](monitor)
+  def caching[L <: AnyRef, V <: Bindable[L]](monitor: V): Bindable.Aux[L, V#Bound] = new CachingMonitor[L, V](monitor)
+
 }
