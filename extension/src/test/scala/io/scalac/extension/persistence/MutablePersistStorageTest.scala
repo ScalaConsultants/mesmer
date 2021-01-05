dc646c730be669798bf9dc2861ea227965bccfb9
Piotr Jósiak <piotr.josiak@scalac.io>
2021-01-05 10:35:49 +0100

    Add MutablePersistingStorageTest

---
+++ extension/src/test/scala/io/scalac/extension/persistence/MutablePersistStorageTest.scala
@@ -0,0 +1,67 @@
+package io.scalac.extension.persistence
+
+import java.util.concurrent.ConcurrentHashMap
+
+import io.scalac.extension.event.PersistenceEvent.{ PersistingEventFinished, PersistingEventStarted }
+import io.scalac.extension.persistence.PersistStorage.PersistEventKey
+import io.scalac.extension.util.TestOps
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.collection.concurrent.{ Map => CMap }
+import scala.jdk.CollectionConverters._
+
+class MutablePersistStorageTest extends AnyFlatSpec with Matchers with TestOps {
+  type Fixture = (CMap[PersistEventKey, PersistingEventStarted], MutablePersistStorage)
+  def test(body: Fixture => Any): Any = {
+    val buffer = new ConcurrentHashMap[PersistEventKey, PersistingEventStarted]()
+    val sut    = new MutablePersistStorage(buffer.asScala)
+    Function.untupled(body)(buffer.asScala, sut)
+  }
+
+  "MutablePersistStorage" should "add started events to internal buffer" in test {
+    case (buffer, sut) =>
+      val events = List.fill(10) {
+        val id = createUniqueId
+        PersistingEventStarted(s"/some/path/${id}", id, 0, System.currentTimeMillis())
+      }
+      events.foreach(sut.persistEventStarted)
+
+      buffer should have size (events.size)
+      buffer.values should contain theSameElementsAs (events)
+  }
+
+  it should "remove started event from internal buffer when corresponding finish event is fired" in test {
+    case (buffer, sut) =>
+      val events = List.fill(10) {
+        val id = createUniqueId
+        PersistingEventStarted(s"/some/path/${id}", id, 0, System.currentTimeMillis())
+      }
+      events.foreach(sut.persistEventStarted)
+      val finished = events
+        .take(5)
+        .map(started =>
+          PersistingEventFinished(started.path, started.persistenceId, started.sequenceNr, started.timestamp + 1000L)
+        )
+
+      finished.foreach(sut.persistEventFinished)
+
+      buffer should have size (events.size - finished.size)
+      buffer.values should contain theSameElementsAs (events.drop(5))
+  }
+
+  it should "return same storage instance with correct latency" in test {
+    case (_, sut) =>
+      val id              = createUniqueId
+      val startTimestamp  = System.currentTimeMillis()
+      val path            = s"/some/path/${id}"
+      val seqNo           = 199
+      val expectedLatency = 1234L
+      sut.persistEventStarted(PersistingEventStarted(path, id, seqNo, startTimestamp))
+      val Some((resultStorage, latency)) =
+        sut.persistEventFinished(PersistingEventFinished(path, id, seqNo, startTimestamp + expectedLatency))
+      resultStorage should be theSameInstanceAs (sut)
+      latency should be(expectedLatency)
+  }
+
+}
