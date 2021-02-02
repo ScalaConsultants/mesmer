a9c2816a3112201eaecf395623c5e099ecb5e96e
Piotr Jósiak <piotr.josiak@scalac.io>
2021-02-02 10:58:31 +0100

    Improve CommonRegexPathService

--- extension/src/main/scala/io/scalac/extension/service/CommonRegexPathService.scala
+++ extension/src/main/scala/io/scalac/extension/service/CommonRegexPathService.scala
@@ -2,21 +2,58 @@ package io.scalac.extension.service
 
 import io.scalac.extension.model.Path
 
+import scala.annotation.tailrec
+
 object CommonRegexPathService extends PathService {
   private val uuid   = """^[\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12}$""".r
   private val number = """^[+-]?\d+\.?\d*$""".r
 
-  val numberTemplate         = "{num}"
-  val uuidTemplate           = "{uuid}"
-  private val detectionChain = List((number, numberTemplate), (uuid, uuidTemplate))
+  val numberTemplate = "{num}"
+  val uuidTemplate   = "{uuid}"
 
-  override def template(path: Path): Path =
-    path
-      .split('/')
-      .map { segment =>
-        detectionChain.find {
-          case (regex, _) => regex.findPrefixOf(segment).isDefined
-        }.map(_._2).getOrElse(segment)
+  override def template(path: Path): Path = {
+
+    @tailrec
+    def replaceInPath(offset: Int, replacements: Vector[(Int, Int, String)]): Vector[(Int, Int, String)] = {
+      var nextIndex = path.indexOf('/', offset)
+      if (nextIndex <= -1) {
+        nextIndex = path.size
       }
-      .mkString("", "/", if (path.endsWith("/")) "/" else "")
+      if (offset >= path.size) {
+        replacements
+      } else {
+        path.substring(offset, nextIndex) match {
+          case number(_*) => {
+            //next must be slash, so skip it
+            replaceInPath(nextIndex + 1, replacements :+ (offset, nextIndex, numberTemplate))
+          }
+          case subs if subs.length == 36 => {
+            subs match {
+              case uuid(_*) => {
+                //next must be slash, so skip it
+                replaceInPath(nextIndex + 1, replacements :+ (offset, nextIndex, uuidTemplate))
+              }
+              case _ => replaceInPath(nextIndex + 1, replacements)
+            }
+          }
+          case _ => replaceInPath(nextIndex + 1, replacements)
+        }
+      }
+    }
+
+    replaceInPath(0, Vector.empty) match {
+      case Vector() => path
+      case replacements => {
+        val builder = new StringBuilder(path.size)
+        val last = replacements.foldLeft(0) {
+          case (begin, repl) => {
+            builder.append(path.substring(begin, repl._1)).append(repl._3)
+            repl._2
+          }
+        }
+        builder.append(path.substring(last, path.length))
+        builder.toString()
+      }
+    }
+  }
 }
