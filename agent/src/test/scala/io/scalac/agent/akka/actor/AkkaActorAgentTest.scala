44d797018a460acdb826718284c8f58ff59e8421
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2021-02-24 14:40:06 -0300

    Fixing agent loading for actor agent tests

--- agent/src/test/scala/io/scalac/agent/akka/actor/AkkaActorAgentTest.scala
+++ agent/src/test/scala/io/scalac/agent/akka/actor/AkkaActorAgentTest.scala
@@ -12,10 +12,8 @@ import akka.actor.typed.scaladsl.adapter._
 import akka.actor.typed.scaladsl.{ ActorContext, Behaviors, StashBuffer }
 import akka.{ actor => classic }
 
-import net.bytebuddy.ByteBuddy
 import net.bytebuddy.agent.ByteBuddyAgent
 import net.bytebuddy.agent.builder.AgentBuilder
-import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpecLike
 
 import io.scalac.extension.actorServiceKey
@@ -24,25 +22,14 @@ import io.scalac.extension.event.ActorEvent.StashMeasurement
 import io.scalac.extension.util.ReceptionistOps
 
 class AkkaActorAgentTest
-    extends ScalaTestWithActorTestKit(classic.ActorSystem("AkkaActorAgentTest").toTyped)
+    extends ScalaTestWithActorTestKit({
+      AkkaActorAgentTest.loadAgent()
+      classic.ActorSystem("AkkaActorAgentTest").toTyped
+    })
     with AnyFlatSpecLike
-    with ReceptionistOps
-    with BeforeAndAfterAll {
+    with ReceptionistOps {
 
   import AkkaActorAgentTest._
-
-  override def beforeAll(): Unit = {
-    super.beforeAll()
-    val instrumentation = ByteBuddyAgent.install()
-    val builder =
-      new AgentBuilder.Default()
-        .`with`(AgentBuilder.Listener.StreamWriting.toSystemOut.withErrorsOnly())
-        .`with`(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
-        .`with`(AgentBuilder.TypeStrategy.Default.REDEFINE)
-        .`with`(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
-    val modules = Map(AkkaActorAgent.moduleName -> AkkaActorAgent.defaultVersion)
-    AkkaActorAgent.agent.installOn(builder, instrumentation, modules)
-  }
 
   def test(body: Fixture => Any): Any = {
     val monitor = createTestProbe[ActorEvent]
@@ -98,6 +85,15 @@ class AkkaActorAgentTest
 }
 
 object AkkaActorAgentTest {
+
+  def loadAgent(): Unit = {
+    val instrumentation = ByteBuddyAgent.install()
+    val builder =
+      new AgentBuilder.Default()
+        .`with`(AgentBuilder.Listener.StreamWriting.toSystemOut.withErrorsOnly())
+    val modules = Map(AkkaActorAgent.moduleName -> AkkaActorAgent.defaultVersion)
+    AkkaActorAgent.agent.installOn(builder, instrumentation, modules)
+  }
 
   import scala.language.reflectiveCalls
 
