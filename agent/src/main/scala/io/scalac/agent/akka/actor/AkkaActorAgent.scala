45e56dd5210be4e10cc08852fcf0ffb1d7c13e57
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2021-02-25 09:40:05 -0300

    Removing unwanted comments

--- agent/src/main/scala/io/scalac/agent/akka/actor/AkkaActorAgent.scala
+++ agent/src/main/scala/io/scalac/agent/akka/actor/AkkaActorAgent.scala
@@ -2,7 +2,6 @@ package io.scalac.agent.akka.actor
 
 import akka.actor.typed.Behavior
 
-import net.bytebuddy.ByteBuddy
 import net.bytebuddy.asm.Advice
 import net.bytebuddy.description.`type`.TypeDescription
 import net.bytebuddy.description.method.MethodDescription
@@ -74,16 +73,6 @@ object AkkaActorAgent {
                   )
               )
             )
-//            .method(
-//              named[MethodDescription]("stash")
-//                .or(named[MethodDescription]("clear"))
-//                .or(
-//                  named[MethodDescription]("unstash")
-//                  // since there're two `unstash` methods, we need to specify parameter types
-//                    .and(takesArguments(classOf[Behavior[_]], classOf[Int], classOf[Function1[_, _]]))
-//                )
-//            )
-//            .intercept(advice)
         }
         .installOn(instrumentation)
       LoadingResult(targetClassName)
