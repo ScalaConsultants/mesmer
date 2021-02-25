dc8215afe5238526992512da60121f5b781a31a6
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2021-02-25 09:53:07 -0300

    Higher relationship tests with override approach

--- core/src/test/scala/io/scalac/extension/util/TestOps.scala
+++ core/src/test/scala/io/scalac/extension/util/TestOps.scala
@@ -9,9 +9,15 @@ trait TestOps {
 
   def createUniqueId: String = UUID.randomUUID().toString
 
-  def sameOrParent(ref: ActorRef[_]): Matcher[ActorRef[_]] = left => {
-    val test = left.path.toStringWithoutAddress.startsWith(ref.path.toStringWithoutAddress)
-    MatchResult(test, s"${ref} is not same or parent of ${left}", s"${ref} is same as or parent of ${left}")
+  def sameOrParent(parent: ActorRef[_]): Matcher[ActorRef[_]] = ref => {
+    MatchResult(
+      testSameOrParent(ref, parent),
+      s"${parent} is not same or parent of ${ref}",
+      s"${parent} is same as or parent of ${ref}"
+    )
   }
+
+  protected def testSameOrParent(ref: ActorRef[_], parent: ActorRef[_]): Boolean =
+    parent.path == ref.path || ref.path.parent == parent.path
 
 }
