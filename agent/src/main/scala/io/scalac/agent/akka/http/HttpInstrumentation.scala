74ad1190e8d1793c1dbfeb5d678e805fbd0d7d6f
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2021-03-26 14:44:10 -0300

    Extract to BidiFlowForward

--- agent/src/main/scala/io/scalac/agent/akka/http/HttpInstrumentation.scala
+++ agent/src/main/scala/io/scalac/agent/akka/http/HttpInstrumentation.scala
@@ -1,25 +1,26 @@
 package io.scalac.agent.akka.http
 
+import java.lang.reflect.Method
+import java.util.UUID
+
+import scala.concurrent.Future
+
 import _root_.akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
 import _root_.akka.http.scaladsl.settings.ServerSettings
-import _root_.akka.stream.{ Attributes, BidiShape, FlowShape, Inlet, Materializer, Outlet }
+import _root_.akka.stream.{ BidiShape, Materializer }
 import akka.actor.typed.scaladsl.adapter._
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.{ ConnectionContext, HttpExt }
 import akka.stream.scaladsl.{ BidiFlow, Broadcast, Flow, GraphDSL, Source, Zip }
 
+import net.bytebuddy.implementation.bind.annotation._
+
+import io.scalac.core.akka.stream.BidiFlowForward
 import io.scalac.core.model._
 import io.scalac.core.util.Timestamp
 import io.scalac.extension.event.EventBus
 import io.scalac.extension.event.HttpEvent._
-import net.bytebuddy.implementation.bind.annotation._
-import java.lang.reflect.Method
-import java.util.UUID
-
-import scala.concurrent.Future
-
-import akka.stream.stage.{ GraphStage, GraphStageLogic, InHandler, OutHandler }
 
 class HttpInstrumentation
 object HttpInstrumentation {
@@ -38,63 +39,9 @@ object HttpInstrumentation {
 
     val system = self.asInstanceOf[HttpExt].system.toTyped
 
-    val connectionsCountFlow = BidiFlow.fromGraph(
-      new GraphStage[BidiShape[HttpRequest, HttpRequest, HttpResponse, HttpResponse]] {
-
-        private val requestIn   = Inlet.create[HttpRequest]("request.in")
-        private val requestOut  = Outlet.create[HttpRequest]("request.out")
-        private val responseIn  = Inlet.create[HttpResponse]("response.in")
-        private val responseOut = Outlet.create[HttpResponse]("response.out")
-
-        override val shape: BidiShape[HttpRequest, HttpRequest, HttpResponse, HttpResponse] =
-          BidiShape(requestIn, requestOut, responseIn, responseOut)
-
-        override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
-          // This following `setHandler`s are just to forward requests and responses.
-          // The real value of this flow is `preStart` and postStop` hooks override.
-          // The general handlers and its interactions is based on Kamon's akka instrumentation.
-
-          setHandler(
-            requestIn,
-            new InHandler {
-              override def onPush(): Unit           = push(requestOut, grab(requestIn))
-              override def onUpstreamFinish(): Unit = complete(requestOut)
-            }
-          )
-
-          setHandler(
-            requestOut,
-            new OutHandler {
-              override def onPull(): Unit                             = pull(requestIn)
-              override def onDownstreamFinish(cause: Throwable): Unit = cancel(requestIn)
-            }
-          )
-
-          setHandler(
-            responseIn,
-            new InHandler {
-              override def onPush(): Unit           = push(responseOut, grab(responseIn))
-              override def onUpstreamFinish(): Unit = completeStage()
-            }
-          )
-
-          setHandler(
-            responseOut,
-            new OutHandler {
-              override def onPull(): Unit                             = pull(responseIn)
-              override def onDownstreamFinish(cause: Throwable): Unit = cancel(responseIn)
-            }
-          )
-
-          override def preStart(): Unit =
-            EventBus(system).publishEvent(ConnectionStarted(interface, port))
-
-          override def postStop(): Unit =
-            EventBus(system).publishEvent(ConnectionCompleted(interface, port))
-
-        }
-
-      }
+    val connectionsCountFlow = BidiFlowForward[HttpRequest, HttpResponse](
+      onPreStart = () => EventBus(system).publishEvent(ConnectionStarted(interface, port)),
+      onPostStop = () => EventBus(system).publishEvent(ConnectionCompleted(interface, port))
     )
 
     val requestIdFlow =
