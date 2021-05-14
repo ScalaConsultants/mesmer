4edcf533831ea526717b5c3bef38a4be32269670
Scala Steward <me@scala-steward.org>
2021-05-14 20:15:53 +0200

    Update byte-buddy, byte-buddy-agent to 1.10.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,8 +30,8 @@ object Dependencies {
   )
 
   val byteBuddy = Seq(
-    "net.bytebuddy" % "byte-buddy"       % "1.10.18",
-    "net.bytebuddy" % "byte-buddy-agent" % "1.10.18"
+    "net.bytebuddy" % "byte-buddy"       % "1.10.22",
+    "net.bytebuddy" % "byte-buddy-agent" % "1.10.22"
   )
 
 
