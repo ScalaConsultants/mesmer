8c5a54e61dd1ff9a0ecfeaf97600aeaa4e1e8386
Scala Steward <me@scala-steward.org>
2021-05-18 01:42:22 +0200

    Update byte-buddy, byte-buddy-agent to 1.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,8 +32,8 @@ object Dependencies {
   )
 
   val byteBuddy = Seq(
-    "net.bytebuddy" % "byte-buddy"       % "1.10.22",
-    "net.bytebuddy" % "byte-buddy-agent" % "1.10.22"
+    "net.bytebuddy" % "byte-buddy"       % "1.11.0",
+    "net.bytebuddy" % "byte-buddy-agent" % "1.11.0"
   )
 
   val logback = Seq("ch.qos.logback" % "logback-classic" % LogbackVersion)
