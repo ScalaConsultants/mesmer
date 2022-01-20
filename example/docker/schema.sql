95656d901f3e32a98f08e6a44a50a94da82bc33e
Łukasz Gajowy <lukasz.gajowy@scalac.io>
2022-01-20 13:42:12 +0100

    Format sql code
    This is to stay concise. pre-commit tool will soon enforce that.

--- example/docker/schema.sql
+++ example/docker/schema.sql
@@ -1,53 +1,55 @@
-drop table IF EXISTS public.journal;
+drop table if exists public.journal;
 
-drop table IF EXISTS public.snapshot;
+drop table if exists public.snapshot;
 
-CREATE TABLE IF NOT EXISTS public.event_journal(
-  ordering BIGSERIAL,
-  persistence_id VARCHAR(255) NOT NULL,
-  sequence_number BIGINT NOT NULL,
-  deleted BOOLEAN DEFAULT FALSE NOT NULL,
+create table if not exists public.event_journal
+(
+    ordering bigserial, --noqa: disable=L029
+    persistence_id varchar(255) not null,
+    sequence_number bigint not null,
+    deleted boolean default false not null,
 
-  writer VARCHAR(255) NOT NULL,
-  write_timestamp BIGINT,
-  adapter_manifest VARCHAR(255),
+    writer varchar(255) not null,
+    write_timestamp bigint,
+    adapter_manifest varchar(255),
 
-  event_ser_id INTEGER NOT NULL,
-  event_ser_manifest VARCHAR(255) NOT NULL,
-  event_payload BYTEA NOT NULL,
+    event_ser_id integer not null,
+    event_ser_manifest varchar(255) not null,
+    event_payload bytea not null,
 
-  meta_ser_id INTEGER,
-  meta_ser_manifest VARCHAR(255),
-  meta_payload BYTEA,
+    meta_ser_id integer,
+    meta_ser_manifest varchar(255),
+    meta_payload bytea,
 
-  PRIMARY KEY(persistence_id, sequence_number)
+    primary key (persistence_id, sequence_number)
 );
 
-CREATE UNIQUE INDEX event_journal_ordering_idx ON public.event_journal(ordering);
+CREATE UNIQUE INDEX event_journal_ordering_idx ON public.event_journal (ordering); -- noqa: PRS
 
-CREATE TABLE IF NOT EXISTS public.event_tag(
-    event_id BIGINT,
-    tag VARCHAR(256),
-    PRIMARY KEY(event_id, tag),
-    CONSTRAINT fk_event_journal
-      FOREIGN KEY(event_id)
-      REFERENCES event_journal(ordering)
-      ON DELETE CASCADE
+create table if not exists public.event_tag
+(
+    event_id bigint,
+    tag varchar(256),
+    primary key (event_id, tag),
+    constraint fk_event_journal
+    foreign key (event_id)
+    references event_journal (ordering)
+    on delete cascade
 );
 
-CREATE TABLE IF NOT EXISTS public.snapshot (
-  persistence_id VARCHAR(255) NOT NULL,
-  sequence_number BIGINT NOT NULL,
-  created BIGINT NOT NULL,
+create table if not exists public.snapshot
+(
+    persistence_id varchar(255) not null,
+    sequence_number bigint not null,
+    created bigint not null,
 
-  snapshot_ser_id INTEGER NOT NULL,
-  snapshot_ser_manifest VARCHAR(255) NOT NULL,
-  snapshot_payload BYTEA NOT NULL,
+    snapshot_ser_id integer not null,
+    snapshot_ser_manifest varchar(255) not null,
+    snapshot_payload bytea not null,
 
-  meta_ser_id INTEGER,
-  meta_ser_manifest VARCHAR(255),
-  meta_payload BYTEA,
+    meta_ser_id integer,
+    meta_ser_manifest varchar(255),
+    meta_payload bytea,
 
-  PRIMARY KEY(persistence_id, sequence_number)
+    primary key (persistence_id, sequence_number)
 );
-
