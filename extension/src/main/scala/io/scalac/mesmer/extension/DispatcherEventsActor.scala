df463d19e1945e2c107a6f00bfd8305e10c89a63
ornel-lloyd-edano <ornel.lloyd.edano@gmail.com>
2022-03-04 16:51:11 +0400

    implement dispatcher event actor

--- extension/src/main/scala/io/scalac/mesmer/extension/DispatcherEventsActor.scala
+++ extension/src/main/scala/io/scalac/mesmer/extension/DispatcherEventsActor.scala
@@ -1,11 +1,16 @@
 package io.scalac.mesmer.extension
 
-import akka.actor.typed.Behavior
-
+import akka.actor.typed.receptionist.Receptionist
+import akka.actor.typed.{Behavior, PostStop}
+import akka.actor.typed.receptionist.Receptionist.Deregister
+import akka.actor.typed.receptionist.Receptionist.Register
+import akka.actor.typed.scaladsl.Behaviors
 import io.scalac.mesmer.core.event.DispatcherEvent
+import io.scalac.mesmer.core.event.DispatcherEvent.ExecutorConfigEvent
+import io.scalac.mesmer.core.dispatcherServiceKey
 import io.scalac.mesmer.core.model.Node
+import io.scalac.mesmer.extension.DispatcherEventsActor.Event.DispatcherEventWrapper
 import io.scalac.mesmer.extension.metric.DispatcherStaticMetricsMonitor
-import io.scalac.mesmer.extension.service.PathService
 
 object DispatcherEventsActor {
 
@@ -17,7 +22,28 @@ object DispatcherEventsActor {
 
   def apply(
     dispatcherMetricMonitor: DispatcherStaticMetricsMonitor,
-    pathService: PathService,
-    node: Option[Node] = None
-  ): Behavior[Event] = ???
+    node: Option[Node]
+  ): Behavior[Event] =
+    Behaviors.setup[Event] { ctx =>
+
+      Receptionist(ctx.system).ref ! Register(dispatcherServiceKey, ctx.messageAdapter(DispatcherEventWrapper.apply))
+
+      val receptionistAdapter = ctx.messageAdapter(DispatcherEventWrapper.apply)
+
+      def createDispatcherAttributes(event: ExecutorConfigEvent): DispatcherStaticMetricsMonitor.Attributes =
+        DispatcherStaticMetricsMonitor.Attributes(node, event.minThreads, event.maxThreads, event.parallelismFactor)
+
+      Behaviors
+        .receiveMessagePartial[Event] {
+          case DispatcherEventWrapper(event: ExecutorConfigEvent) =>
+            dispatcherMetricMonitor.bind(createDispatcherAttributes(event))
+            Behaviors.same
+          case _=>
+            Behaviors.same
+        }.receiveSignal { case (_, PostStop) =>
+        ctx.log.info("Dispatcher events monitor terminated")
+        Receptionist(ctx.system).ref ! Deregister(dispatcherServiceKey, receptionistAdapter)
+        Behaviors.same
+      }
+    }
 }
