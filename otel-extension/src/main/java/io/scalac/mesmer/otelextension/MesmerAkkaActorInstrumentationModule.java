a7e45716758e5387896f67f5a16b359a1358152a
Łukasz Gajowy <lukasz.gajowy@scalac.io>
2022-03-08 10:50:07 +0100

    Minor: formatting fix

--- otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaActorInstrumentationModule.java
+++ otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaActorInstrumentationModule.java
@@ -4,17 +4,16 @@ import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.scalac.mesmer.agent.akka.actor.AkkaActorAgent;
-
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)
 public class MesmerAkkaActorInstrumentationModule extends InstrumentationModule {
-    public MesmerAkkaActorInstrumentationModule() {
-        super("mesmer-akka-actor");
-    }
+  public MesmerAkkaActorInstrumentationModule() {
+    super("mesmer-akka-actor");
+  }
 
-    @Override
-    public List<TypeInstrumentation> typeInstrumentations() {
-        return AkkaActorAgent.agent().asOtelTypeInstrumentations();
-    }
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return AkkaActorAgent.agent().asOtelTypeInstrumentations();
+  }
 }
--- otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaPersistenceInstrumentationModule.java
+++ otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaPersistenceInstrumentationModule.java
@@ -4,17 +4,16 @@ import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.scalac.mesmer.agent.akka.persistence.AkkaPersistenceAgent;
-
 import java.util.List;
 
 @AutoService(InstrumentationModule.class)
 public class MesmerAkkaPersistenceInstrumentationModule extends InstrumentationModule {
-    public MesmerAkkaPersistenceInstrumentationModule() {
-        super("mesmer-akka-persistence");
-    }
+  public MesmerAkkaPersistenceInstrumentationModule() {
+    super("mesmer-akka-persistence");
+  }
 
-    @Override
-    public List<TypeInstrumentation> typeInstrumentations() {
-        return AkkaPersistenceAgent.agent().asOtelTypeInstrumentations();
-    }
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return AkkaPersistenceAgent.agent().asOtelTypeInstrumentations();
+  }
 }
