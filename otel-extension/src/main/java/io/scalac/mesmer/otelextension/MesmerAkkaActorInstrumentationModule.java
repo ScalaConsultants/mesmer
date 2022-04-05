531141026142e1c88d14f03df74b92f53cd3fd30
Piotr Josiak <piotr.josiak@scalac.io>
2022-04-05 21:09:08 +0200

    Uncomment other instrumentation modules

--- otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaActorInstrumentationModule.java
+++ otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaActorInstrumentationModule.java
@@ -1,19 +1,19 @@
-//package io.scalac.mesmer.otelextension;
-//
-//import com.google.auto.service.AutoService;
-//import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
-//import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
-//import io.scalac.mesmer.agent.akka.actor.AkkaActorAgent;
-//import java.util.List;
-//
-//@AutoService(InstrumentationModule.class)
-//public class MesmerAkkaActorInstrumentationModule extends InstrumentationModule {
-//  public MesmerAkkaActorInstrumentationModule() {
-//    super("mesmer-akka-actor");
-//  }
-//
-//  @Override
-//  public List<TypeInstrumentation> typeInstrumentations() {
-//    return AkkaActorAgent.agent().asOtelTypeInstrumentations();
-//  }
-//}
+package io.scalac.mesmer.otelextension;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.scalac.mesmer.agent.akka.actor.AkkaActorAgent;
+import java.util.List;
+
+@AutoService(InstrumentationModule.class)
+public class MesmerAkkaActorInstrumentationModule extends InstrumentationModule {
+  public MesmerAkkaActorInstrumentationModule() {
+    super("mesmer-akka-actor");
+  }
+
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return AkkaActorAgent.agent().asOtelTypeInstrumentations();
+  }
+}
