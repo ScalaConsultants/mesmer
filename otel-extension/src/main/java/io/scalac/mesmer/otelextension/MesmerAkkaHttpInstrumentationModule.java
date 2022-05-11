dd6ee6a808b28de35996a0dd1dbfc3e703337990
Łukasz Gajowy <lukasz.gajowy@scalac.io>
2022-05-11 12:38:41 +0200

    Implement muzzle interface in instrumentation module to keep the agent happy

--- otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaHttpInstrumentationModule.java
+++ otel-extension/src/main/java/io/scalac/mesmer/otelextension/MesmerAkkaHttpInstrumentationModule.java
@@ -1,14 +1,22 @@
 package io.scalac.mesmer.otelextension;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
+
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.tooling.muzzle.InstrumentationModuleMuzzle;
+import io.opentelemetry.javaagent.tooling.muzzle.VirtualFieldMappingsBuilder;
+import io.opentelemetry.javaagent.tooling.muzzle.references.ClassRef;
 import io.scalac.mesmer.otelextension.instrumentations.akka.http.AkkaHttpAgent;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @AutoService(InstrumentationModule.class)
-public class MesmerAkkaHttpInstrumentationModule extends InstrumentationModule {
+public class MesmerAkkaHttpInstrumentationModule extends InstrumentationModule
+    implements InstrumentationModuleMuzzle {
   public MesmerAkkaHttpInstrumentationModule() {
     super("mesmer-akka-http");
   }
@@ -25,5 +33,18 @@ public class MesmerAkkaHttpInstrumentationModule extends InstrumentationModule {
         "io.scalac.mesmer.otelextension.instrumentations.akka.http.AkkaHttpConnectionsInstrumentation$HttpConnectionInstruments$",
         "io.scalac.mesmer.otelextension.instrumentations.akka.http.AkkaHttpConnectionsInstrumentation",
         "io.scalac.mesmer.instrumentation.http.HttpExtConnectionAdvice");
+  }
+
+  @Override
+  public Map<String, ClassRef> getMuzzleReferences() {
+    return emptyMap();
+  }
+
+  @Override
+  public void registerMuzzleVirtualFields(VirtualFieldMappingsBuilder builder) {}
+
+  @Override
+  public List<String> getMuzzleHelperClassNames() {
+    return emptyList();
   }
 }
