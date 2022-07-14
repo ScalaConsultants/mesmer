fb1e83014defaa37842c0a9459abd2354cf8fdc2
Łukasz Gajowy <lukasz.gajowy@scalac.io>
2022-07-14 19:07:15 +0200

    Test forwarding counters provided by zio

--- otel-extension/src/it/scala/io/scalac/mesmer/otelextension/instrumentations/zio/ZIOMetricsTest.scala
+++ otel-extension/src/it/scala/io/scalac/mesmer/otelextension/instrumentations/zio/ZIOMetricsTest.scala
@@ -1,6 +1,6 @@
 package io.scalac.mesmer.otelextension.instrumentations.zio
 
-import io.opentelemetry.sdk.metrics.data.MetricDataType
+import io.opentelemetry.sdk.metrics.data.{ MetricData, MetricDataType }
 import io.scalac.mesmer.agent.utils.OtelAgentTest
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
@@ -14,18 +14,56 @@ class ZIOMetricsTest extends OtelAgentTest with AnyFlatSpecLike with Matchers {
   "OTEL counter" should "be registered and working for a custom ZIO Counter" in {
     val counter = Metric.counter("my_custom_zio_counter").fromConst(1)
 
-    val testProgram: ZIO[Any, Nothing, Unit] = (for {
+    val testProgram = (for {
       _ <- ZIO.unit @@ counter
     } yield ()).repeatN(19)
 
-    Unsafe.unsafe { implicit u =>
-      Runtime.default.unsafe.runToFuture(testProgram)
+    runUnsafely(testProgram, runtimeMetrics = false)
 
-      assertMetrics("mesmer_zio_forwarded_my_custom_zio_counter") {
-        case data if data.getType == MetricDataType.DOUBLE_SUM =>
-          val value: Option[Double] = data.getDoubleSumData.getPoints.asScala.map(_.getValue).toList.headOption
-          value.get should be(20)
-      }
-    }
+    assertCounterMetricValue("mesmer_zio_forwarded_my_custom_zio_counter", 20)
   }
+
+  "Runtime fiber_started metric" should "be picked up by our OTEL instrumentations" in {
+    val testProgram = for {
+      fiber  <- ZIO.succeed(1).fork
+      fiber2 <- ZIO.succeed(1).fork
+      _      <- fiber.join
+      _      <- fiber2.join
+    } yield ()
+
+    runUnsafely(testProgram, runtimeMetrics = true)
+
+    assertCounterMetricValue("mesmer_zio_forwarded_zio_fiber_started", 2)
+    assertCounterMetricValue("mesmer_zio_forwarded_zio_fiber_successes", 2)
+  }
+
+  "Runtime fiber_failures metric" should "be picked up by our OTEL instrumentations" in {
+    val testProgram =
+      for {
+        fiber  <- ZIO.fail(new Throwable("I failed.")).fork
+        fiber2 <- ZIO.fail(new Throwable("I failed as well.")).fork
+        _      <- fiber.join
+        _      <- fiber2.join
+      } yield ()
+
+    runUnsafely(testProgram, runtimeMetrics = true)
+
+    assertCounterMetricValue("mesmer_zio_forwarded_zio_fiber_failures", 2)
+  }
+
+  private def runUnsafely[E <: Throwable, A](
+    testProgram: ZIO[Any, E, A],
+    runtimeMetrics: Boolean
+  ): CancelableFuture[Any] = {
+    val program = if (runtimeMetrics) testProgram.provide(Runtime.enableRuntimeMetrics) else testProgram
+    Unsafe.unsafe(implicit u => Runtime.default.unsafe.runToFuture(program))
+  }
+
+  private def assertCounterMetricValue(metricName: String, value: Double): Unit =
+    assertMetrics(metricName) {
+      case data if data.getType == MetricDataType.DOUBLE_SUM => getCounterValue(data).get should be(value)
+    }
+
+  private def getCounterValue(data: MetricData): Option[Double] =
+    data.getDoubleSumData.getPoints.asScala.map(_.getValue).toList.headOption
 }
