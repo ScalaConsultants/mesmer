281b441ed5bd92e678a1772b64702e9337a3c0d2
Łukasz Gajowy <lukasz.gajowy@scalac.io>
2023-02-16 15:30:58 +0100

    Replace Atomic references with volatile vars
    That's more self-explanatory - we need only common visibility between threads

--- otel-extension/src/main/scala/io/scalac/mesmer/otelextension/instrumentations/akka/stream/AkkaStreamMetrics.scala
+++ otel-extension/src/main/scala/io/scalac/mesmer/otelextension/instrumentations/akka/stream/AkkaStreamMetrics.scala
@@ -1,7 +1,5 @@
 package io.scalac.mesmer.otelextension.instrumentations.akka.stream
 
-import java.util.concurrent.atomic.AtomicReference
-
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.metrics.Meter
 
@@ -11,18 +9,17 @@ final class AkkaStreamMetrics(node: Option[Node]) {
   private val meter: Meter = GlobalOpenTelemetry.getMeter("mesmer")
   private val attributes   = AkkaStreamAttributes.forNode(node)
 
-  private val runningStreamsTotal = new AtomicReference[Option[Long]](None)
-  private val runningActorsTotal  = new AtomicReference[Option[Long]](None)
+  @volatile private var runningStreamsTotal: Option[Long] = None
+  @volatile private var runningActorsTotal: Option[Long]  = None
 
   meter.gaugeBuilder("mesmer_akka_streams_running_streams").ofLongs().buildWithCallback { measurement =>
-    runningStreamsTotal.get.foreach(v => measurement.record(v, attributes))
+    runningStreamsTotal.map(v => measurement.record(v, attributes))
   }
 
   meter.gaugeBuilder("mesmer_akka_streams_actors").ofLongs().buildWithCallback { measurement =>
-    runningActorsTotal.get.foreach(v => measurement.record(v, attributes))
+    runningActorsTotal.map(v => measurement.record(v, attributes))
   }
 
-  def setRunningStreamsTotal(value: Long): Unit = runningStreamsTotal.set(Some(value))
-  def setRunningActorsTotal(value: Long): Unit  = runningActorsTotal.set(Some(value))
-
+  def setRunningStreamsTotal(value: Long): Unit = runningStreamsTotal = Some(value)
+  def setRunningActorsTotal(value: Long): Unit  = runningActorsTotal = Some(value)
 }
